53f65a059645c174e6cb977c5bad8bc6edf1595e
Eduardo Chaves <henriqueduardo2002@gmail.com>
2023-02-01 20:37:36 -0300

    preparation -> setting a multi page app

--- eduardo/app.py
+++ eduardo/app.py
@@ -1,29 +1,15 @@
-import pandas as pd
-import plotly.express as px
-from dash import Dash, html, dcc, dash_table
+from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 
 app = Dash(
   __name__,
   external_stylesheets=[dbc.themes.ZEPHYR, dbc.icons.FONT_AWESOME],
   meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+  use_pages=True,
 )
 
-######### CREATING GLOBAL VARS #########
-
-df = pd.read_pickle('data/online-retail(cleaned).pkl')
-
-######### LAYOUT VARIABLES #########
-
 app.layout = html.Div([
-  html.Div([
-    html.I(className="fa-solid fa-gauge"),
-    html.H1('Procenge Data Dashboard'),
-  ]),
-  
-  dash_table.DataTable(df.head(10).to_dict('records'))
 ])
-
 
 if __name__ == '__main__':
   app.run_server(debug=True)
